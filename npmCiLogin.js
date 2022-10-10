5edc6f9d92433d7a2d107049520e075cf5e93140
Srikanth Anusuri <sanusuri@starbucks.com>
2022-10-10 14:57:49 -0700

    Use axios to fetch token

--- npmCiLogin.js
+++ npmCiLogin.js
@@ -19,61 +19,50 @@ const init = () => {
       throw new Error('Missing environment variables.');
     }
   }
-  
-  const login = (username, password, registry = 'https://registry.npmjs.org') => {
-    validate(username, password);
-    const https = require('https');
-    const registryUrl = new URL(`${registry}/-/user/org.couchdb.user:${username}`)
-    const data = { name: username, password };
-    const options = {
-      hostname: registryUrl.hostname,
-      port: 443,
-      path: registryUrl.pathname,
-      method: 'PUT',
-      headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+
+  const fetchToken = async (registry, data) => {
+    const registryUrl = new URL(`${registry}/-/user/org.couchdb.user:${data.username}`)
+    const axios = require('axios');
+    const response = await axios.put(registryUrl, data, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         }
-    };
-    const req = https.request(options, (res) => {
-      console.log('Response from registry', registry);
-      console.log('statusCode:', res.statusCode);
-      if(res.statusCode > 299 || res.statusCode < 200) {
-        res.on('data', (d) => {
-            const body = JSON.parse(d.toString());
-            console.error('Error:', body.error || body.message || body);
-            throw new Error(`Could not fetch token from the registry [${registry}]`);
-        })
-      }
-    
-      res.on('data', (d) => {
-        const { token } = JSON.parse(d.toString());
-        const key = registry.trim().substring(registryUrl.protocol.length) + '/:_authToken';
-    
-        const { spawn } = require('child_process');
-        const npmLogin = spawn('npm', ['config', '--global', 'set', `${key}=${token}`]);
-        let consoleOutput = '';
-        npmLogin.stderr.on('data', (data) => {
-          consoleOutput += data.toString("utf-8");
-        });
-        npmLogin.on('close', () => {
-          if(/ERR!/.test(consoleOutput)) {
-            console.error(`User ${username} could not login to npm registry ${registry}`);
-            console.error(consoleOutput)
-            throw new Error(`User ${username} could not login to npm registry ${registry}`)
-          } else {
-            console.info(`User ${username} successfully logged into the npm registry ${registry}`);
-          }
-        });
-      });
+    });
+    console.log('Response from registry', registry);
+    console.log('statusCode:', response.status);
+    const key = registry.trim().substring(registryUrl.protocol.length) + '/:_authToken';
+    const token = response.data.token;
+    return {key, token};
+  };
+
+  const setToken = async (key, token) => {
+    const { spawn } = require('child_process');
+    const npmLogin = spawn('npm', ['config', '--global', 'set', `${key}=${token}`]);
+    let consoleOutput = '';
+
+    npmLogin.stderr.on('data', (data) => {
+        consoleOutput += data.toString("utf-8");
     });
     
-    req.on('error', (e) => {
-      console.error(`Could not retrieve the token from the registry ${registry}`, e);
+    npmLogin.on('close', () => {
+        if(/ERR!/.test(consoleOutput)) {
+        console.error(`User ${username} could not login to npm registry ${registry}`);
+        console.error(consoleOutput)
+        throw new Error(`User ${username} could not login to npm registry ${registry}`)
+        } else {
+        console.info(`User ${username} successfully logged into the npm registry ${registry}`);
+        }
     });
+  };
+  
+  const login = async (username, password, registry = 'https://registry.npmjs.org') => {
+    validate(username, password);
     
-    req.write(JSON.stringify(data));
-    req.end();
+    const data = { name: username, password };
+    const {key, token} = await fetchToken(registry, data);
+
+    await setToken(key, token);
   };
   
   module.exports = login;
